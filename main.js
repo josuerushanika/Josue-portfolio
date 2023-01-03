f6b386a2983f478046445afb668cb1d7f529c518
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-01-03 00:03:20 -0800

    Add js fonction

---
+++ main.js
@@ -0,0 +1,13 @@
+const mobileMenu = document.querySelector("nav .mobile-menu");
+const mobileClose = document.querySelector("nav .mobile-close");
+const mobileNavigation = document.querySelector(".mobile-navigation");
+
+mobileMenu.addEventListener("click", () => {
+    mobileNavigation.classList.add("active");
+    mobileClose.classList.add("active");
+})
+
+mobileClose.addEventListener("click", () => {
+    mobileNavigation.classList.remove("active");
+    mobileClose.classList.remove("active");
+})
