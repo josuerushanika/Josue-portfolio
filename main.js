3738d4ae8961140f249e5b92d2dd5d5f95c80dc9
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-01-03 10:15:44 -0800

    Fix linters

--- main.js
+++ main.js
@@ -1,30 +1,11 @@
-// const mobileMenu = document.querySelector('nav .mobile-menu');
-// const mobileClose = document.querySelector('nav .mobile-close');
-// const mobileNavigation = document.querySelector('.mobile-navigation');
-
-// mobileMenu.addEventListener('click', () => {
-//   mobileNavigation.classList.add('active');
-//   mobileClose.classList.add('active');
-// });
-
-// mobileClose.addEventListener('click', () => {
-//   mobileNavigation.classList.remove('active');
-//   mobileClose.classList.remove('active');
-// });
-
-// function display() {
- 
-// }
-
 const mobileMenu = document.querySelector('nav .mobile-menu');
 const mobileClose = document.querySelector('nav .mobile-close');
 const mobileNavigation = document.querySelector('.mobile-navigation');
 
-
-function display(){
+function display() {
   mobileNavigation.classList.toggle('active');
   mobileClose.classList.toggle('active');
 }
 
 mobileMenu.addEventListener('click', display);
-mobileClose.addEventListener('click', display)
+mobileClose.addEventListener('click', display);
