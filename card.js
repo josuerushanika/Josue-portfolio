54794bcdd80407739da813ee7074401759771e38
josuerushanika <josuerushanika@gmail.com>
2023-08-03 10:22:54 +0200

    Update files

--- card.js
+++ card.js
@@ -1,17 +1,17 @@
 const projects = [
   {
     id: 1,
-    title: 'Tonic',
-    image: 'images/portfolio-1.svg',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    title: 'conference website',
+    image: 'images/project.png',
+    description: 'A conference website is a dedicated online platform that provides information and resources related to a specific conference or event. It serves as a central hub for attendees, organizers, and interested individuals to access details about the conference, such as the schedule, agenda, speakers, registration information, venue location, and other relevant information.',
     technologies: ['html', 'css', 'javaScript'],
-    source: 'https://github.com/josuerushanika/Josue-portfolio',
-    live: 'https://josuerushanika.github.io/Josue-portfolio/',
+    source: 'https://github.com/josuerushanika/Rwanda-Conference',
+    live: 'https://josuerushanika.github.io/Rwanda-Conference/index.html',
   },
   {
     id: 2,
     title: 'Multi-Post Stories',
-    image: 'images/portfolio-2.svg',
+    image: 'images/movies.png',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     technologies: ['html', 'css', 'javaScript'],
     source: 'https://github.com/josuerushanika/Josue-portfolio',
